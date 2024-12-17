1b0f304d81545101d985384e37393b177662ded8
Léon Frenot <leon.frenot@ens-lyon.fr>
2024-12-17 13:24:33 +0000

    Larger struct test file

--- test/lean/struct.expected.lean
+++ test/lean/struct.expected.lean
@@ -4,10 +4,16 @@ open Sail
 
 structure My_struct where
   field1 : Int
-  field2 : Int
+  field2 : (BitVec 1)
 
 def undefined_My_struct (lit : Unit) : SailM My_struct :=
   return sorry
+
+def struct_field2 (s : My_struct) : (BitVec 1) :=
+  s.field2
+
+def struct_update_field2 (s : My_struct) (b : (BitVec 1)) : My_struct :=
+  {s with field2 := b}
 
 def initialize_registers : Unit :=
   ()
