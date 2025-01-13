b30421f228bc9ae70f7c8db5d14aadebf489d7aa
Jakob von Raumer <jakob@von-raumer.de>
2025-01-13 14:40:10 +0100

    add placeholer for state monad to stateful functions

--- test/lean/enum.expected.lean
+++ test/lean/enum.expected.lean
@@ -5,7 +5,7 @@ open Sail
 inductive E where | A | B | C
   deriving Inhabited
 
-def undefined_E : SailM E :=
+def undefined_E : SailM E := do
   return (sorry : E)
 
 def initialize_registers : Unit :=
