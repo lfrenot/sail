829d1d3f401a3f16cf66d2899853d54ac4ea81e3
Léon Frenot <leon.frenot@ens-lyon.fr>
2025-01-14 13:39:47 +0000

    Adding test

---
+++ test/lean/reg.expected.lean
@@ -0,0 +1,16 @@
+import Reg.Sail.Sail
+
+open Sail
+
+class MonadReg where
+  set_R0: BitVec 64 -> SailM Unit
+  get_R0: SailM (BitVec 64)
+
+variable [MonadReg]
+
+open MonadReg
+
+def initialize_registers : SailM Unit := do
+  let w__0 := (undefined_bitvector 64)
+  set_R0 w__0
+  return ()
