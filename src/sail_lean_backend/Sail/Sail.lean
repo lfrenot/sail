a1e5fc59c7f88cf77ad3a4b688cd0d8735eb796a
Léon Frenot <leon.frenot@ens-lyon.fr>
2025-01-15 16:43:07 +0000

    Fixing Lean style in Sail.lean

--- src/sail_lean_backend/Sail/Sail.lean
+++ src/sail_lean_backend/Sail/Sail.lean
@@ -19,23 +19,23 @@ def truncate {w : Nat} (x : BitVec w) (w' : Nat) : BitVec w' :=
 def truncateLsb {w : Nat} (x : BitVec w) (w' : Nat) : BitVec w' :=
   x.extractLsb' (w - w') w'
 
-def extractLsb {w: Nat} (x: BitVec w) (hi lo: Nat) : BitVec (hi - lo + 1) :=
+def extractLsb {w : Nat} (x : BitVec w) (hi lo : Nat) : BitVec (hi - lo + 1) :=
   x.extractLsb hi lo
 
-def update_subrange' {w: Nat} (x: BitVec w) (start len: Nat) (y: BitVec len) : BitVec w :=
+def updateSubrange' {w : Nat} (x : BitVec w) (start len : Nat) (y : BitVec len) : BitVec w :=
   let mask := ~~~(((BitVec.allOnes len).zeroExtend w) <<< start)
   let y' := mask ||| ((y.zeroExtend w) <<< start)
   x &&& y'
 
-def update_subrange {w: Nat} (x: BitVec w) (hi lo: Nat) (y: BitVec (hi - lo + 1)) : BitVec w :=
-  update_subrange' x lo _ y
+def updateSubrange {w : Nat} (x : BitVec w) (hi lo : Nat) (y : BitVec (hi - lo + 1)) : BitVec w :=
+  updateSubrange' x lo _ y
 
 end BitVec
 end Sail
 
-structure registerRef (regstate regval a: Type) where
-  name: String
-  read_from: regstate -> a
-  write_to: a -> regstate -> regstate
-  of_regval: regval -> Option a
-  regval_of: a -> regval
+structure registerRef (regstate regval a : Type) where
+  name : String
+  read_from : regstate -> a
+  write_to : a -> regstate -> regstate
+  of_regval : regval -> Option a
+  regval_of : a -> regval
--- test/lean/bitfield.expected.lean
+++ test/lean/bitfield.expected.lean
@@ -14,54 +14,54 @@ def _get_cr_type_bits (v : cr_type) : BitVec 8 :=
   (Sail.BitVec.extractLsb v.bits (HSub.hSub 8 1) 0)
 
 def _update_cr_type_bits (v : cr_type) (x : BitVec 8) : cr_type :=
-  {v with bits := (Sail.BitVec.update_subrange v.bits (HSub.hSub 8 1) 0 x)}
+  {v with bits := (Sail.BitVec.updateSubrange v.bits (HSub.hSub 8 1) 0 x)}
 
-def _set_cr_type_bits (r_ref : register_ref Unit Unit cr_type) (v : BitVec 8) : Unit :=
+def _set_cr_type_bits (r_ref : registerRef Unit Unit cr_type) (v : BitVec 8) : Unit :=
   sorry
 
 def _get_cr_type_CR0 (v : cr_type) : BitVec 4 :=
   (Sail.BitVec.extractLsb v.bits 7 4)
 
 def _update_cr_type_CR0 (v : cr_type) (x : BitVec 4) : cr_type :=
-  {v with bits := (Sail.BitVec.update_subrange v.bits 7 4 x)}
+  {v with bits := (Sail.BitVec.updateSubrange v.bits 7 4 x)}
 
-def _set_cr_type_CR0 (r_ref : register_ref Unit Unit cr_type) (v : BitVec 4) : Unit :=
+def _set_cr_type_CR0 (r_ref : registerRef Unit Unit cr_type) (v : BitVec 4) : Unit :=
   sorry
 
 def _get_cr_type_CR1 (v : cr_type) : BitVec 2 :=
   (Sail.BitVec.extractLsb v.bits 3 2)
 
 def _update_cr_type_CR1 (v : cr_type) (x : BitVec 2) : cr_type :=
-  {v with bits := (Sail.BitVec.update_subrange v.bits 3 2 x)}
+  {v with bits := (Sail.BitVec.updateSubrange v.bits 3 2 x)}
 
-def _set_cr_type_CR1 (r_ref : register_ref Unit Unit cr_type) (v : BitVec 2) : Unit :=
+def _set_cr_type_CR1 (r_ref : registerRef Unit Unit cr_type) (v : BitVec 2) : Unit :=
   sorry
 
 def _get_cr_type_CR3 (v : cr_type) : BitVec 2 :=
   (Sail.BitVec.extractLsb v.bits 1 0)
 
 def _update_cr_type_CR3 (v : cr_type) (x : BitVec 2) : cr_type :=
-  {v with bits := (Sail.BitVec.update_subrange v.bits 1 0 x)}
+  {v with bits := (Sail.BitVec.updateSubrange v.bits 1 0 x)}
 
-def _set_cr_type_CR3 (r_ref : register_ref Unit Unit cr_type) (v : BitVec 2) : Unit :=
+def _set_cr_type_CR3 (r_ref : registerRef Unit Unit cr_type) (v : BitVec 2) : Unit :=
   sorry
 
 def _get_cr_type_GT (v : cr_type) : BitVec 1 :=
   (Sail.BitVec.extractLsb v.bits 6 6)
 
 def _update_cr_type_GT (v : cr_type) (x : BitVec 1) : cr_type :=
-  {v with bits := (Sail.BitVec.update_subrange v.bits 6 6 x)}
+  {v with bits := (Sail.BitVec.updateSubrange v.bits 6 6 x)}
 
-def _set_cr_type_GT (r_ref : register_ref Unit Unit cr_type) (v : BitVec 1) : Unit :=
+def _set_cr_type_GT (r_ref : registerRef Unit Unit cr_type) (v : BitVec 1) : Unit :=
   sorry
 
 def _get_cr_type_LT (v : cr_type) : BitVec 1 :=
   (Sail.BitVec.extractLsb v.bits 7 7)
 
 def _update_cr_type_LT (v : cr_type) (x : BitVec 1) : cr_type :=
-  {v with bits := (Sail.BitVec.update_subrange v.bits 7 7 x)}
+  {v with bits := (Sail.BitVec.updateSubrange v.bits 7 7 x)}
 
-def _set_cr_type_LT (r_ref : register_ref Unit Unit cr_type) (v : BitVec 1) : Unit :=
+def _set_cr_type_LT (r_ref : registerRef Unit Unit cr_type) (v : BitVec 1) : Unit :=
   sorry
 
 def initialize_registers : Unit :=
